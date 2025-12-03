f14aecf3f92b065f0bae3ded3094d18a7b2ae8cb
satoshi <mbakgminc@gmail.com>
2025-12-03 22:49:35 +0900

    init.luaの不要行削除

--- init.lua
+++ init.lua
@@ -91,9 +91,3 @@ require 'kickstart.lazy'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
--- ここまでkickstart.nvim
--- ここから自作
-require 'keymaps'
---
---
